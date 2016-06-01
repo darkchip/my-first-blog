28e33fd9f88ca6ec2b007bc6d5b4089d69ab0196
Kamal Robinson <krobinson@afsenyc.org>
2016-06-01 15:32:05 -0400

    My Django Girls app, first commit

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,11 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:3]
     return render(request, 'blog/post_list.html', {'posts': posts})
     
 def user_post_list(request, pk):
-    posts = Post.objects.filter(author__pk=pk, published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(author__pk=pk, published_date__lte=timezone.now()).order_by('-published_date')
     author = User.objects.get(pk=pk)
     return render(request, 'blog/user_detail.html', {'posts': posts, 'author': author})
     
